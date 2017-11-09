1b395c1deca8f4dc3f7330640d99b586caba986a
Victor de la Cruz <vcg_cruz@hotmail.com>
2017-11-08 23:08:56 -0600

    entornos de desarrollo y produccion

---
+++ src/environment/dev.ts
@@ -0,0 +1,5 @@
+
+export const DEV = {
+  REST_API_URL:"http://localhost:8082/",
+  ENV_NAME:'DEVELOP'
+};
---
+++ src/environment/prod.ts
@@ -0,0 +1,5 @@
+
+export const PROD = {
+  REST_API_URL:"http://213.136.92.15:8082/",
+  ENV_NAME:'PRODUCTION'
+};
